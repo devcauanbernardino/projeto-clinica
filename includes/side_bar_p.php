cbe038ba82c1dd43138cebe65a19728e7e73e789
devcauanbernardino <devcauanbernardino@gmail.com>
2026-01-29 16:58:51 -0300

    referencia do sidebar

---
+++ includes/side_bar_p.php
@@ -0,0 +1,48 @@
+<!-- SIDEBAR -->
+    <aside class="sidebar d-flex flex-column p-4">
+
+        <div class="d-flex align-items-center mb-5">
+            <img src="../assets/img/logo-sem-fundo.png" width="48" class="me-2">
+            <div>
+                <h4 class="mb-0 fw-bold">MedFlow</h4>
+                <small class="text-uppercase text-soft fw-bold"
+                    style="font-size: 10px; letter-spacing: 1px;">
+                    Portal do Paciente
+                </small>
+            </div>
+        </div>
+
+        <nav class="nav nav-pills flex-column mb-auto">
+            <a class="nav-link active" href="#">
+                <span class="material-symbols-outlined">home</span> Início
+            </a>
+
+            <a class="nav-link" href="minhas_consultas.php">
+                <span class="material-symbols-outlined">calendar_month</span> Minhas Consultas
+            </a>
+
+            <a class="nav-link" href="#">
+                <span class="material-symbols-outlined">description</span> Prontuário
+            </a>
+
+            <a class="nav-link" href="#">
+                <span class="material-symbols-outlined">lab_panel</span> Exames
+            </a>
+
+            <a class="nav-link" href="#">
+                <span class="material-symbols-outlined">person</span> Perfil
+            </a>
+        </nav>
+
+        <div class="profile-section d-flex align-items-center mt-4">
+            <img class="rounded-circle me-3"
+                src="https://i.pravatar.cc/100"
+                width="40" height="40">
+
+            <div>
+                <p class="mb-0 fw-bold small">Ricardo Silva</p>
+                <p class="mb-0 text-soft" style="font-size: 11px;">Paciente</p>
+            </div>
+        </div>
+
+    </aside>
